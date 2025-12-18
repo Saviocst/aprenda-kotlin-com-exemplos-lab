e642c53e775e42ec58710bca839859e7bff4b616
Savio Costa <saviocosta.flam20@gmail.com>
2025-12-18 16:56:18 -0300

    Alteração 1

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    //val inscritos = mutableSet<Usuario>()
+
+    private val alunos = mutableSetOf<Usuario>()
+
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        alunos.add(usuario)
+        println("Aluno ${usuario.nome} matriculado com sucesso!")
     }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val conteud1 = ConteudoEducacional("Kotlin Basico", 10)
+    val conteudo2 = ConteudoEducacional("Kotlin POO", 7)
+
+    val aluno1 = Usuario("Savio", "savio@gmail.com")
+
+    val formacao = Formacao("Android Developer", Nivel.BASICO, listOf(conteud1, conteudo2))
+
+    println(formacao.matricular(aluno1))
 }
